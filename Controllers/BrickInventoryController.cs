66474b9249b1a04d4b41ddb42480919b46404cf0
kinnes36 <kinnes36@yahoo.com>
2023-02-18 15:00:49 -0500

    first commit

---
+++ Controllers/BrickInventoryController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class BrickInventoryControler : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> GetLegoStuff()
+    {
+        return null;
+    }
+}
